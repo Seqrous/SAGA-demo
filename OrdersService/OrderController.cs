9714a3f7c3bcebdd6fc856ac813693ddf381676f
seqrous <krzysiek.ziach@gmail.com>
2026-01-01 14:04:19 +0100

    Added postgres and connection test.

--- OrdersService/OrderController.cs
+++ OrdersService/OrderController.cs
@@ -1,14 +1,22 @@
 using Microsoft.AspNetCore.Mvc;
+using Npgsql;
 
 namespace OrdersService;
 
 [ApiController]
 [Route("[controller]")]
-public class OrderController : ControllerBase
+public class OrderController(NpgsqlDataSource dataSource) : ControllerBase
 {
     [HttpPost]
-    public ActionResult Create()
+    public async Task<ActionResult> Create()
     {
+        await using var connection = await dataSource.OpenConnectionAsync();
+        await using var command = new NpgsqlCommand("SELECT 1", connection);
+        await using var reader = await command.ExecuteReaderAsync();
+
+        if (!reader.HasRows)
+            return NotFound();
+        
         return Ok();
     }
 }
--- OrdersService/Program.cs
+++ OrdersService/Program.cs
@@ -1,3 +1,5 @@
+using Npgsql;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +7,15 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
+
+var pgHost = Environment.GetEnvironmentVariable("POSTGRES_HOST");
+var pgDbName = Environment.GetEnvironmentVariable("POSTGRES_DB");
+var pgUser = Environment.GetEnvironmentVariable("POSTGRES_USER");
+var pgPassword = Environment.GetEnvironmentVariable("POSTGRES_PASSWORD");
+var pgPort = Environment.GetEnvironmentVariable("POSTGRES_PORT");
+
+await using var dataSource = NpgsqlDataSource.Create($"HOST={pgHost};Username={pgUser};Password={pgPassword};Database={pgDbName};Port={pgPort};");
+builder.Services.AddSingleton(dataSource);
 
 var app = builder.Build();
 
