468c1a8a24b97e28aaf80064c8ffadb66f0b05b0
seqrous <krzysiek.ziach@gmail.com>
2026-01-03 21:12:51 +0100

    DB config adjusted and added init SQL.

---
+++ init.sql
@@ -0,0 +1,18 @@
+CREATE SCHEMA IF NOT EXISTS orchestrator;
+
+CREATE TABLE IF NOT EXISTS orchestrator.SagaLog (
+    saga_id uuid PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    status VARCHAR(32) NOT NULL,
+    current_step VARCHAR(32) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS orchestrator.SagaStep (
+    saga_id UUID NOT NULL,
+    step_name VARCHAR(32) NOT NULL,
+    status VARCHAR(32) NOT NULL,
+    idempotency_key UUID NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (saga_id, step_name)
+);
